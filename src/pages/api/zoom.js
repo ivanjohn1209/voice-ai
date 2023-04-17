61c7d37e1ffc7b44490e912122f0161c87cfe111
John Ivan <pagdayawonjohnivan@gmail.com>
2023-04-17 09:52:30 +0800

    update

--- src/pages/api/zoom.js
+++ src/pages/api/zoom.js
@@ -1,36 +1,33 @@
-import axios from 'axios';
-import crypto from 'crypto';
-
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-        const { headers, body } = req;
-        const event = headers['x-zoom-event'];
-        const signature = headers['x-zoom-signature'];
-        const expectedSignature = crypto
-            .createHmac('sha256', process.env.ZOOM_API_SECRET)
-            .update(`${process.env.ZOOM_VERIFICATION_TOKEN}${headers['x-zoom-timestamp']}${JSON.stringify(body)}`)
-            .digest('base64');
+        const event = req.body;
+        if (event.event === 'meeting.started') {
+            // 3. Handle the "meeting.started" event from Zoom, which will include the meeting ID and other relevant information.
+            const meetingId = event.payload.object.id;
 
-        if (signature !== expectedSignature) {
-            return res.status(401).json({ message: 'Invalid signature' });
+            // 4. Use the Zoom API to start recording the meeting.
+            const recordingResponse = await startRecording(meetingId);
+
+            // 5. Use a transcription service (such as Whisper AI or similar) to transcribe the audio from the recording.
+            const transcription = await transcribeRecording(recordingResponse.recording_id);
+
+            // 6. Once the transcription is complete, send the results to a destination of your choice (such as an email or a database).
+            await sendTranscription(transcription);
         }
-
-        switch (event) {
-            case 'meeting.started':
-                // handle meeting started event
-                break;
-            case 'meeting.ended':
-                // handle meeting ended event
-                break;
-            case 'recording.completed':
-                // handle recording completed event
-                break;
-            default:
-                return res.status(400).json({ message: 'Unknown event type' });
-        }
-
-        return res.status(200).json({ message: 'Event processed successfully' });
+        res.status(200).json({ success: true });
     } else {
-        return res.status(405).json({ message: 'Method not allowed' });
+        res.status(404).send('Not Found');
     }
+}
+
+async function startRecording(meetingId) {
+    // Use the Zoom API to start recording the meeting and return the response.
+}
+
+async function transcribeRecording(recordingId) {
+    // Use a transcription service (such as Whisper AI or similar) to transcribe the audio from the recording and return the result.
+}
+
+async function sendTranscription(transcription) {
+    // Send the transcription to a destination of your choice (such as an email or a database).
 }

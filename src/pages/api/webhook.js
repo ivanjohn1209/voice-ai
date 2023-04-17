2bd2e8147885799059d4d53c82e0c36f0bbd4377
John Ivan <pagdayawonjohnivan@gmail.com>
2023-04-17 20:45:17 +0800

    update

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,36 +1,47 @@
 import { createHash } from 'crypto'
 
 export default async (req, res) => {
-    // Verify the request signature
-    const signature = req.headers['x-zm-signature']
-    const payload = await getPayload(req)
-    const expectedSignature = createHash('sha256')
-        .update(process.env.ZOOM_API_SECRET + payload)
-        .digest('base64')
-    if (signature !== expectedSignature) {
-        return res.status(401).send('Unauthorized')
+    if (req.method === 'GET') {
+        // Respond to the validation request
+        const params = req.query
+        const verifyToken = process.env.ZOOM_VERIFICATION_TOKEN
+        if (params.verify_token === verifyToken) {
+            res.status(200).json({ verify_token: params.verify_token })
+        } else {
+            res.status(401).send('Unauthorized')
+        }
+    } else {
+        // Verify the request signature
+        const signature = req.headers['x-zm-signature']
+        const payload = await getPayload(req)
+        const expectedSignature = createHash('sha256')
+            .update(process.env.ZOOM_API_SECRET + payload)
+            .digest('base64')
+        if (signature !== expectedSignature) {
+            return res.status(401).send('Unauthorized')
+        }
+
+        // Check if the event is a recording completed event
+        const event = req.body.event
+        if (event !== 'recording.completed') {
+            return res.status(200).send('OK')
+        }
+
+        // Get the cloud recording download URL
+        const recording = req.body.payload.object
+        const recordingId = recording.uuid
+        const recordingDownloadUrl = await getRecordingDownloadUrl(recordingId)
+
+        // Transcribe the audio using the OpenAI API
+        // const transcription = await transcribeAudio(recordingDownloadUrl)
+
+        // Handle the transcription result
+        // console.log('Transcription result:', transcription)
+        // TODO: Do something with the transcription result
+        console.log(recordingDownloadUrl)
+        // Return a success response
+        res.status(200).send('OK')
     }
-
-    // Check if the event is a recording completed event
-    const event = req.body.event
-    if (event !== 'recording.completed') {
-        return res.status(200).send('OK')
-    }
-
-    // Get the cloud recording download URL
-    const recording = req.body.payload.object
-    const recordingId = recording.uuid
-    const recordingDownloadUrl = await getRecordingDownloadUrl(recordingId)
-
-    // Transcribe the audio using the OpenAI API
-    // const transcription = await transcribeAudio(recordingDownloadUrl)
-
-    // Handle the transcription result
-    // console.log('Transcription result:', transcription)
-    // TODO: Do something with the transcription result
-    console.log(recordingDownloadUrl)
-    // Return a success response
-    res.status(200).send('OK')
 }
 
 async function getPayload(req) {
